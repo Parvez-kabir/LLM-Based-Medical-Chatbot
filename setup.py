0428f034d576e58cccddf17c03a98cab5205a51c
parvez-kabir <pkobir6@gmail.com>
2025-11-12 03:01:38 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Parvez Kabir",
+    author_email="kabir15-5539@diu.edu.bd",
+    packages=find_packages(),
+    install_requires=[]
+)
